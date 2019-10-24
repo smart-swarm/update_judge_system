0a12eab4459c6aa6fd666e1fa5dfa42f87453e5f
guozhenglong <guozhenglong.cn@gmail.com>
2019-10-25 00:13:12 +0800

    update the judge system.

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Download the latest file and use the script to update judge system
+```
+git clone https://github.com/smart-swarm/update_judge_system.git
+cd update_judge_system
+chmod +x update.sh 
+./update.sh
+```
---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "start uninstalling the old judge system..."
+sudo apt remove ros-kinetic-judge-system
+echo "start installing the new judge system..."
+sudo dpkg -i ros-kinetic-judge-system_0.0.0-0xenial_amd64.deb
+echo "new judge system installed."
+
