8d21133cada6adcb1a6c006a2a59736be005c20d
guozhenglong <guozhenglong.cn@gmail.com>
2019-11-05 01:46:12 +0800

    v1.4, fixed a bug calculated score .

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 echo "start uninstalling the old judge system..."
 sudo apt remove ros-kinetic-judge-system
 echo "start installing the new judge system..."
-sudo dpkg -i ros-kinetic-judge-system_1.2.0-0xenial_amd64.deb
+sudo dpkg -i ros-kinetic-judge-system-latest_0xenial_amd64.deb
 echo "new judge system installed."
 
